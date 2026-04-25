1580bfb589b932eb2b75f66d171dec08a4a53d4b
ainaraskaz <ainaras448@gmail.com>
2026-04-25 18:10:47 +0300

    linker

---
+++ links.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s ~/dotfiles/niri ~/.config/niri
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/sway ~/.config/sway
+ln -s ~/dotfiles/swaylock ~/.config/swaylock
+ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s ~/dotfiles/wallpapers ~/.config/wallpapers
+ln -s ~/dotfiles/waybar ~/.config/waybar
